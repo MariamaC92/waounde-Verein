48e594e27f01a97771a3dc597ae86b564a7f92cd
MariamaC92 <122444096+MariamaC92@users.noreply.github.com>
2023-04-20 13:38:57 +0200

    Routing Nav

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,3 +1,18 @@
+import Header from "./Header/Header";
+import MainNavigation from "./MainNavigation/MainNavigation";
+import {Route, Routes} from "react-router-dom"
+
+
 export default function App() {
-  return (<></>);
+  return (<>
+  <MainNavigation />
+
+ <Routes>
+  <Route path="/" >
+  <Route index element={<Header />} />
+  <Route path="/hallo" element={<Header />} />
+  </Route>
+  
+ </Routes>
+  </>);
 }
