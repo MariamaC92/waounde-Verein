39469434a2fba6fd72e90fccc79b9313efe5e97a
Uwe Wieben <u.wieben@cloud-user-skills.de>
2023-04-25 14:10:55 +0200

    FIX GITIGNORE

---
+++ ui/src/_App/App.js
@@ -0,0 +1,19 @@
+import "./CSS/App.css"; 
+import Header from "../Header/Header";
+import MainNavigation from "../MainNavigation/MainNavigation";
+import {Route, Routes} from "react-router-dom"
+
+
+export default function App() {
+  return (<>
+  <MainNavigation /> 
+
+ <Routes>
+  <Route path="/" >
+  <Route index element={<Header />} />
+  <Route path="/hallo" element={<Header />} />
+  </Route>
+  
+ </Routes>
+  </>);
+}
