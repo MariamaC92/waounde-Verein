6aaac4ad5dbe25c7e23fab362a90aefc78e71401
MariamaC92 <122444096+MariamaC92@users.noreply.github.com>
2023-04-27 17:27:21 +0200

    Routing Fix

--- api/server.js
+++ api/server.js
@@ -1,8 +1,10 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import contentPageRouter from "./ContentPage/contentPageRouter.js";
 
-dotenv.config();
+dotenv.config({path: './database/.env'});
+console.log(process.env.PGHOST);
 const port = process.env.port || process.env.PORT || 5000;
 const app = express();
 
@@ -10,8 +12,10 @@ app.use(express.json());
 app.use(cors({ origin: /http:\/\/localhost/ }));
 app.options("*", cors());
 
+app.use('/contentpage', contentPageRouter);
+
 app.listen(port, () => {
-  console.log("Waoundé listen on ${port}");
+  console.log(`Waoundé listen on ${port}`);
 });
 
-app.use('/contentPage', contentPageRouter);
+
