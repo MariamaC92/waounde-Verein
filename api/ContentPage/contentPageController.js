cc133959b951513980d1a226e5f428c09b4342cb
Lukas <git@github.com>
2023-04-28 11:28:13 +0200

    Content-Komponente implementiert

--- api/ContentPage/contentPageController.js
+++ api/ContentPage/contentPageController.js
@@ -1,10 +1,14 @@
-import pool from "../database/Pool.js"
+import pool from "../database/Pool.js";
 
 export const getContentPage = async (req, res, next) => {
-    const {rows} = await pool.query("select * from content_page");
-    res.json({rows});
-}
-
-
-
-
+  const { rows } = await pool.query("select * from content_page");
+  const headline = rows.map((item) => {
+    console.log(item);
+    return({
+      headcription: item.headline_description,
+      deepness: item.deepness,
+    });
+  });
+  console.log(headline);
+  res.json({ headline });
+};
