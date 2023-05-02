029da1acfaa239c8cb7f58ad84a0283a9a0f46dd
MariamaC92 <122444096+MariamaC92@users.noreply.github.com>
2023-05-02 14:09:58 +0200

    Kontaktformular erstellt

--- ui/src/_App/App.js
+++ ui/src/_App/App.js
@@ -2,6 +2,7 @@ import "./CSS/App.css";
 import Header from "../Header/Header";
 import MainNavigation from "../MainNavigation/MainNavigation";
 import {Route, Routes} from "react-router-dom"
+import Kontaktformular from "../Footer/Kontaktformular";
 
 
 export default function App() {
@@ -12,6 +13,8 @@ export default function App() {
   <Route path="/" >
   <Route index element={<Header />} />
   <Route path="/hallo" element={<Header />} />
+  <Route path="/kontakt" element={<Kontaktformular />} />
+
   </Route>
   
  </Routes>
