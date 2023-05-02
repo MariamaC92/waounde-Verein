8ba73583b53f78c55a79cc144bcb596b79e7d05d
MariamaC92 <122444096+MariamaC92@users.noreply.github.com>
2023-05-02 18:04:40 +0200

    Heading Loop

--- ui/src/ContentPage/ContentPage.js
+++ ui/src/ContentPage/ContentPage.js
@@ -4,15 +4,26 @@ import axios from "axios";
 export default function ContentPage() {
  const [content, setContent] = useState();
 
- useEffect(() => {
-    axios 
-        .get("http://localhost:5000/contentpage/")
-        .then ((response) => {setContent(response)}) 
-        .catch((error) => {console.log(error)})
-
- },[]) 
-console.log(content);
-    return (<>
-    <h1></h1>
-    </>)
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/contentpage/")
+      .then((response) => {
+        setContent(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+  console.log(content);
+  return (
+    <>
+      {content &&
+      content.headline.map((item) => {
+        const Heading = `h${item.deepness}`
+        return <Heading>{item.headcription}</Heading>
+      }) }
+      {/* Lukas alleine */}
+      {/*    {content && <p>{content.textcontent.topic}</p>} */}
+    </>
+  );
 }
