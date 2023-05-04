808d6c6d0e45d62bcf83ec0cd6879c1b54021435
Uwe Wieben <u.wieben@cloud-user-skills.de>
2023-05-04 17:04:25 +0200

    First Styling

--- ui/src/Footer/Footer.js
+++ ui/src/Footer/Footer.js
@@ -1,19 +1,16 @@
-//Lukas
-
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Footer.css";
 
-
 export default function Footer() {
   return (
-    <footer className="footerLinks">
+    <footer className="footerBar">
         <ul>
-          <li><Link to="/links">Links</Link> </li>
-          <li><Link to="/kontakt">Kontakt</Link> </li>
-          <li><Link to="/impressum">Impressum</Link></li>
+          <li><Link to="/links" className="footerLink">Links</Link> </li>
+          <li><Link to="/kontakt" className="footerLink">Kontakt</Link> </li>
+          <li><Link to="/datenschutz" className="footerLink">Cookies/Datenschutz</Link> </li>
+          <li><Link to="/impressum" className="footerLink">Impressum</Link></li>
         </ul>
-      
     </footer>
   );
 };
