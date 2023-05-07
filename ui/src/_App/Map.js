938dfaf99dcc8a75d27ac56fe32a24610701d7ce
Lukas <git@github.com>
2023-05-07 11:56:28 +0200

    MaskMarker + Zierleisten

--- ui/src/_App/Map.js
+++ ui/src/_App/Map.js
@@ -4,15 +4,14 @@ import "leaflet/dist/leaflet.css";
 import { Icon } from "leaflet";
 
 
-const position = [15.2641251, -12.8677143];
+const position = [15.2661251, -12.8677143];
 
 const markerIcon = new Icon({
-  iconUrl: require("../_App/Images/marker.png"),
-  iconSize: [35, 45],
+  iconUrl: process.env.PUBLIC_URL + "/images/maskmarker.svg",
+  iconSize: [30, 30],
 });
 
 export default function Map() {
-
   return (
     <MapContainer center={position} zoom={14}>
       <TileLayer
@@ -20,7 +19,7 @@ export default function Map() {
         url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       <Marker position={position} icon={markerIcon}>
-       <Popup>
+        <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
         </Popup>
       </Marker>
