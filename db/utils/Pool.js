0834032bc3bb05a2c0cffdbc41ab37feed22054f
Uwe Wieben <u.wieben@cloud-user-skills.de>
2023-05-08 15:19:24 +0200

    dbupdate

--- db/utils/Pool.js
+++
@@ -1,5 +0,0 @@
-import pg from "pg";
-const {Pool} = pg;
-const {PGHOST, PGUSER, PGDATABASE, PGPASSWORD,PGPORT} = process.env;
-const pool = new Pool({PGHOST, PGUSER, PGDATABASE, PGPASSWORD,PGPORT});
-export default pool;
