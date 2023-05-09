2dde6ec7ff1818c7401d7e2ea1eb46d15630be1f
MariamaC92 <122444096+MariamaC92@users.noreply.github.com>
2023-05-09 11:57:23 +0200

    classnames Row

---
+++ db/model/00013_classnameRow.sql
@@ -0,0 +1,7 @@
+INSERT INTO classnames (id,description, content_type_table) VALUES (10,'default_row', 'rowset');
+
+INSERT INTO classnames (id,description, content_type_table) VALUES (11,'default_col', 'colset');
+
+INSERT INTO classnames (id,description, content_type_table) VALUES (12,'default_close', 'rowset');
+
+INSERT INTO classnames (id,description, content_type_table) VALUES (13,'default_close', 'colset');
