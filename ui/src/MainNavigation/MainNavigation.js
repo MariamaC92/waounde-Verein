61c4ec47fbd94dbf890eac909fe283e5405c8de1
Lukas <git@github.com>
2023-05-09 22:03:45 +0200

    Footer responsiv

--- ui/src/MainNavigation/MainNavigation.js
+++ ui/src/MainNavigation/MainNavigation.js
@@ -20,8 +20,7 @@ export default function MainNavigation() {
             <UberUnsIcon/> 
             </div>
             <div className="navPoints">Über uns</div>
-            
-            
+          
             </NavLink>
           </li>
           <li>
