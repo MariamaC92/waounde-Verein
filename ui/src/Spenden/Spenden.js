b99edade18848862d83690a9d984fa48e0e4c038
Lukas <git@github.com>
2023-05-09 17:24:49 +0200

    Spenden angepasst

--- ui/src/Spenden/Spenden.js
+++ ui/src/Spenden/Spenden.js
@@ -8,7 +8,75 @@ export default function Spenden(){
 <div className="spendenbutton">
     <img src={Spendenbutton} alt="Spendenbutton"></img>
 </div>
-
+<div className="zeileSpenden">
+            <div className="spalteSpenden1">
+        <h3>Spendenaktion</h3>
+        <p>Eine Spendenaktion organisieren</p>
+        </div>
+        <div className="spalteSpenden2">
+        <p>In diesem Jahr, gingen die Bauarbeiten am Mutter-Kind-Zentrum, das 2021 begonnen
+wurde und das dem Gesundheitszentrum angeschlossen sein wird, zügig weiter. Zur Jahreswende
+konnte die Kachelung abgeschlossen und der Innenanstrich begonnen werden. Als Nächstes werden
+die Ausstattung des Zentrums, die Rekrutierung und Einweisung des Personals folgen. Die Eröffnung
+ist für Ende 2023 vorgesehen. Reiselustige sind eingeladen, der Eröffnung beizuwohnen! </p> </div> 
+</div>
+<div className="zeileSpenden">
+            <div className="spalteSpenden1">
+        <h3>Unternehmen</h3>
+        <p>Als Unternehmen spenden</p>
+        </div>
+        <div className="spalteSpenden2">
+        <p>In diesem Jahr, gingen die Bauarbeiten am Mutter-Kind-Zentrum, das 2021 begonnen
+wurde und das dem Gesundheitszentrum angeschlossen sein wird, zügig weiter. Zur Jahreswende
+konnte die Kachelung abgeschlossen und der Innenanstrich begonnen werden. Als Nächstes werden
+die Ausstattung des Zentrums, die Rekrutierung und Einweisung des Personals folgen. Die Eröffnung
+ist für Ende 2023 vorgesehen. Reiselustige sind eingeladen, der Eröffnung beizuwohnen! </p> </div> 
+</div>
+<div className="zeileSpenden">
+            <div className="spalteSpenden1">
+        <h3>Vereine</h3>
+        <p>Als Verein spenden</p>
+        </div>
+        <div className="spalteSpenden2">
+        <p>In diesem Jahr, gingen die Bauarbeiten am Mutter-Kind-Zentrum, das 2021 begonnen
+wurde und das dem Gesundheitszentrum angeschlossen sein wird, zügig weiter. Zur Jahreswende
+konnte die Kachelung abgeschlossen und der Innenanstrich begonnen werden. Als Nächstes werden
+die Ausstattung des Zentrums, die Rekrutierung und Einweisung des Personals folgen. Die Eröffnung
+ist für Ende 2023 vorgesehen. Reiselustige sind eingeladen, der Eröffnung beizuwohnen! </p> </div> 
+</div>
+<div className="zeileSpenden">
+            <div className="spalteSpenden1">
+        <h3>Stiftung</h3>
+        <p>Als Stiftung spenden</p>
+        </div>
+        <div className="spalteSpenden2">
+        <p>In diesem Jahr, gingen die Bauarbeiten am Mutter-Kind-Zentrum, das 2021 begonnen
+wurde und das dem Gesundheitszentrum angeschlossen sein wird, zügig weiter. Zur Jahreswende
+konnte die Kachelung abgeschlossen und der Innenanstrich begonnen werden. Als Nächstes werden
+die Ausstattung des Zentrums, die Rekrutierung und Einweisung des Personals folgen. Die Eröffnung
+ist für Ende 2023 vorgesehen. Reiselustige sind eingeladen, der Eröffnung beizuwohnen! </p> </div> 
+</div>
+<div className="zeileSpenden">
+            <div className="spalteSpenden1">
+        <h3>Sachspenden</h3>
+        </div>
+        <div className="spalteSpenden2">
+        <p>In diesem Jahr, gingen die Bauarbeiten am Mutter-Kind-Zentrum, das 2021 begonnen
+wurde und das dem Gesundheitszentrum angeschlossen sein wird, zügig weiter. Zur Jahreswende
+konnte die Kachelung abgeschlossen und der Innenanstrich begonnen werden. Als Nächstes werden
+die Ausstattung des Zentrums, die Rekrutierung und Einweisung des Personals folgen. Die Eröffnung
+ist für Ende 2023 vorgesehen. Reiselustige sind eingeladen, der Eröffnung beizuwohnen! </p> </div> 
+</div>
+<div className="zeileSpenden">
+            <div className="spalteSpenden1">
+        <h3>Danksagung</h3>
+        </div>
+        <div className="spalteSpenden2">
+        <p>Die Selbsthilfegruppe der Bürger Waoundés in Europa e.V. dankt allen privaten Spendern und unterstützenden Organisationen. Ihre Hilfe hat diese Projekte ermöglicht. Danke für die Hilfe und das Vertrauen. </p>
+        <p className="dank">Besonderer Dank geht an:</p> 
+        <p>Arbeitskreis für Entwicklungspolitik und Selbstbesteuerung e.V. - Aktion Eine-Welt Rottweil e.V. - Aktion Selbstbesteuerung e.V. - Solidaritätsfonds der Dioezese Linz - Engagement Global GmbH - Elektriker-ohne-Grenzen Frankreich - Deutsche Gesellschaft f. internationale Zusammenarbeit GmbH - Georg-Kraus-Stiftung Hagen - Intervox Music Publishing GmbH - Sozialreferat Stadt München - Theodor-Heuss-Gymnasium Hagen </p>
+        </div> 
+</div>
 </>
     );
 }
