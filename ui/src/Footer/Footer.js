eb081dff2445b0c4d944f8483db84742ef4cf536
Anna-Lena Goede <122442781+AnnaLena1337@users.noreply.github.com>
2023-05-10 11:37:08 +0200

    keine Ahnung was ich hier mache

--- ui/src/Footer/Footer.js
+++ ui/src/Footer/Footer.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Footer.css";
 import { ReactComponent as OrnamentTwo } from "../_App/Images/OrnamentTwo.svg";
+import { CookiesIcon, ImpressumIcon, KontaktIcon, LinksIcon } from Footer;
 
 export default function Footer() {
   return (
@@ -9,10 +10,21 @@ export default function Footer() {
     <OrnamentTwo className="ornamentFooter"/>
     <footer className="footerBar">
         <ul>
-          <li><Link to="/links" className="footerLink">Links</Link> </li>
-          <li><Link to="/kontakt" className="footerLink">Kontakt</Link> </li>
-          <li><Link to="/datenschutz" className="footerLink">Cookies/Datenschutz</Link> </li>
-          <li><Link to="/impressum" className="footerLink">Impressum</Link></li>
+          <li>
+            <Link to="/links" className="footerLink">
+            <LinksIcon />
+              Links</Link></li>
+          <li>
+            <Link to="/kontakt" className="footerLink">
+            <KontaktIcon />
+              Kontakt</Link></li>
+          <li>
+            <Link to="/datenschutz" className="footerLink">
+            <CookiesIcon />
+              Cookies/Datenschutz</Link></li>
+          <li><Link to="/impressum" className="footerLink">
+          <ImpressumIcon />
+            Impressum</Link></li>
         </ul>
     </footer>
     </>
