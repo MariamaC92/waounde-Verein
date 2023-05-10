55b9a62d788abadd1bc19179fcee54c93dd5c690
MariamaC92 <122444096+MariamaC92@users.noreply.github.com>
2023-05-10 09:22:46 +0200

    cont projekt1 eingefügt

---
+++ db/model/00017_projekt2.sql
@@ -0,0 +1,49 @@
+INSERT INTO textcontent (topic, texttypes_id, classnames_id) VALUES ('Zu Beginn des Schuljahrs 2021/2022 fehlten dem Berufsbildungszentrum zwei Lehrer. Schreinerei und Kälte- und Klimatechnik waren
+betroffen. Wie es genau zu dem Engpass kam, ist unklar. Laut
+Ministerium gab es einen Lehrermangel. Der Leiter der Berufsschule
+suchte daraufhin selbst und fand kurzfristig zwei Berufsschulabgänger, die sich als „Springer“ (Vacataires) zur Verfügung stellten.
+Die Selbsthilfegruppe und das Zentrum teilten sich die Kosten für ein
+Schuljahr. Die ersten Gehälter wurden aus den verbliebenen Mitteln
+des Sozialfonds bezahlt (900 €). Im Juni 2022 überwies die
+Selbsthilfegruppe weitere 930 € für die Kosten der Springer/
+Vacataires bis Schuljahresende. Die Lehrer erhalten je 228 € pro
+Monat. Anbei ein Bild aus der Klimawerkstatt.', 1, 7);
+
+INSERT INTO textcontent (topic, texttypes_id, classnames_id) VALUES ('Bei der Bezahlung im Jahr 2022 blieb es jedoch nicht. Die Selbsthilfegruppe verfasste einen Brief ans
+Ministerium und verlangte, dass das Ministerium die Lehrer im folgenden Jahr übernehme. So
+geschah es im November 2022. Die beiden jungen Lehrer sind nun fest angestellt und in das
+Ausbildungsprogramm der Berufsschule einbezogen. Geht doch!' 1, 7);
+
+INSERT INTO images (uri, alternative) VALUES ('bericht2022_6.png', 'Schüler und Lehrerin im Klassenraum');
+
+INSERT INTO images (uri, alternative) VALUES ('bericht2022_5.png', 'Fluten vor der Klima und Kälte Gebäude' );
+
+INSERT INTO textcontent (topic, texttypes_id, classnames_id) VALUES ('Weiterhin übernahm die Selbsthilfegruppe die Reparatur des Dachs und des Eingangs zu den
+Werkstätten für Metall bzw. Kälte- & Klimatechnik. Durch einen Orkan am 3. Juli 2020 waren sie stark
+beschädigt worden. Nachdem das Ministerium die Bitte des Zentrums um Reparatur zwei Jahre lang
+ignorierte, entschloss sich die Selbsthilfegruppe im Jahr 2022, die Reparatur selbst in Auftrag zu
+geben. Ein Schaden am Dach zieht weitere Schäden nach sich, z.B. an der Einrichtung des Gebäudes.
+Die Reparatur kostete 765.000 CFA oder 1.166 €. Hiervon waren 670 € Materialkosten und 495 €
+Facharbeiterlohn. Unser Dank an unsere Spender! Und noch ein Blick auf das vom Wind aufgebogene
+Dach der Metallwerkstatt (links) und ein Blick auf die Fluten vor der Klima- & Kältewerkstatt (rechts):', 1, 7);
+
+
+INSERT INTO images (uri, alternative) VALUES ('bericht2022_7.png', 'Männer mit schaufeln am graben');
+
+INSERT INTO textcontent (topic, texttypes_id, classnames_id) VALUES ('Im Sommer 2022 erreichte uns leider auch eine traurige Nachricht. Der
+stellvertretende Leiter des Berufsbildungszentrums, Herr Siaka Sonko,
+starb unerwartet an einem Herzversagen. Er stammte aus dem Süden
+Senegals und war seit 2005 stellvertretender Leiter des Zentrums und
+als solcher für die Finanzen verantwortlich. Die Zusammenarbeit mit
+ihm war eine Freude. Er war genau und zuverlässig. Wir hätten uns
+keinen besseren Partner wünschen können. Er hinterlässt eine Frau
+und fünf schulpflichtige Kinder. Er wird sehnlichst vermisst. Danke für
+Ihre unermüdliche Arbeit für die Berufsschule und für die Schüler
+Waoundes, Monsieur Sonko. Wir werden Sie nie vergessen!', 1, 7);
+
+INSERT INTO images (uri, alternative) VALUES ('bericht2022_8.png', 'Mann in traditioneller Soninke Kleidung');
+
+
+UPDATE images SET alternative = 'Fluten vor der Klima und Kälte Gebäude' WHERE id = 14;
+
+DELETE FROM images WHERE id = 16;
